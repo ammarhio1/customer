532b463e373f5d2c19a26cfecf3856dee7271ca5
Ammar Hasan <ammar.hasan37@qmail.cuny.edu>
2025-04-28 21:03:17 -0400

    Prep For Final

---
+++ src/app/components/CustomersList.jsx
@@ -0,0 +1,14 @@
+export default function CustomersList  () {
+    return (
+      <>
+
+      Add New Customer
+      <p>Jane Doe</p>
+      <p>Plain Joe</p>
+      <p>Train Blow</p>
+      <p>Ash Jhoel</p>
+
+    
+      </>
+    )
+  }
---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import CustomersList from "./components/CustomersList";
+
+export default function Home() {
+  return (
+    <>
+    <h1>All Customers</h1>
+    <CustomersList/>
+    </>
+  );
+}
